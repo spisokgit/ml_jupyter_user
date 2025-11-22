aa8c4098fb9c42b49785fc819dc39b7c401af23c
spisokgit <54783767+spisokgit@users.noreply.github.com>
2025-11-22 10:25:48 +0900

    Update run_root.sh

--- run_root.sh
+++ run_root.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+docker run -it -p 8888:8888 --name z_jupyter -v "$PWD":/workspace/ z_jupyter
 docker run -it -p 7777:7777 -p 8501:8501 -p 8000:8000 --name ml -v "$PWD":/usr/src/  ml_jupyter
